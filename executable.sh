ef9804ad97a76cae4b907e9ca08a2a14a6bb00cc
shambhuKumar <kshambhu67515@gmail.com>
2022-11-09 07:56:29 +0530

    improve with logical operator

--- executable.sh
+++ executable.sh
@@ -6,12 +6,4 @@ arguments content= $*"
 
 script=$1
 
-chmod u+x $script
-
-if [ $? -eq 0 ]
-then
-	echo -e "$script is executable now"
-else
-	echo -e "permission change of $script fail!"
-fi	
-
+chmod u+x $script && echo -e "$script is executable now"
